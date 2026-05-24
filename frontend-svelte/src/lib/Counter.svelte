d1ec972dc72d54c2fe575ab663fb3b23f453d6fb
Arif Mulpratama <arifdata7@gmail.com>
2026-05-24 20:49:12 +0700

    Migrasi frontend dari SvelteKit ke Svelte standalone + Vite

---
+++ frontend-svelte/src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+
+  let count: number = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<Button variant="outline" onclick={increment}>
+  Count is {count}
+</Button>
