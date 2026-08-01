f4b852f3732b3ef02f942aae43492fda31a3c99b
Arif Mulpratama <arifdata7@gmail.com>
2026-08-01 23:03:49 +0700

    style(bmhp): rapikan MasterBMHPPage handler dan indent
    Ekstrak submit handler inline jadi named function, hapus noise
    (trailing semicolon, quote-key, spasi ganda). Behavior tetap.

--- frontend-svelte/src/routes/farmasi/MasterBMHPPage.svelte
+++ frontend-svelte/src/routes/farmasi/MasterBMHPPage.svelte
@@ -1,11 +1,12 @@
 <script>
-  import { Tabs, Tab, TabContent, Column, Button, Modal, TextInput } from "carbon-components-svelte";
+  import { Tabs, Tab, TabContent, Button, Modal, TextInput } from "carbon-components-svelte";
   import { Table, Settings } from "carbon-icons-svelte";
 
   import TabelMasterBMHP from "./master_bmhp/TabelMasterBMHP.svelte";
 
   import { getAuthState, addBMHP } from "../../pb/client.svelte";
   import { notif } from "../../lib/notif.svelte";
+
   let auth = getAuthState();
   let openModal = $state(false);
   let inputBMHP = $state("");
@@ -14,50 +15,43 @@
   function clearFields() {
     inputBMHP = "";
     inputBMHPTags = "";
-  };
+  }
+
+  async function submitBMHP() {
+    let resp = await addBMHP(inputBMHP, inputBMHPTags);
+    openModal = false;
+    clearFields();
+    notif.add({
+      kind: resp.ok ? "success" : "error",
+      subtitle: resp.msg,
+      timeout: 3000,
+    });
+  }
 </script>
 
 {#if auth.isLoggedIn}
-<Tabs fullWidth selected={0}>
-  <Tab label="Master BMHP" icon={Table}/>
-  <Tab label="Tab 2" icon={Settings}/>
-  <svelte:fragment slot="content">
-    <TabContent>
-
-      <Button onclick={() => openModal = true}>Tambah Single Master BMHP</Button>
-      <Modal
-        bind:open={openModal}
-        primaryButtonText="Submit"
-        secondaryButtonText="Cancel"
-        on:click:button--primary={async() => {
-
-            let resp = await addBMHP(inputBMHP, inputBMHPTags);
-            openModal = false;
-            clearFields();
-            notif.add({
-              "kind": resp.ok ? "success" : "error",
-              "subtitle": resp.msg, 
-              "timeout": 3000,
-            });
-          }
-        }
-        on:click:button--secondary={
-          () => {
-              (openModal = false);
-            }
-          }
-      >
-        <TextInput bind:value={inputBMHP} labelText="Nama BMHP" placeholder="Masukkan nama BMHP" />
-        <TextInput bind:value={inputBMHPTags} labelText="Tags" placeholder="pisahkan dengan koma ," />
-      </Modal>
-
-    </TabContent>
-    <TabContent>
-       <!-- <TabelMasterBMHP /> -->
-    </TabContent>
-  </svelte:fragment>
-</Tabs>
-
+  <Tabs fullWidth selected={0}>
+    <Tab label="Master BMHP" icon={Table} />
+    <Tab label="Tab 2" icon={Settings} />
+    <svelte:fragment slot="content">
+      <TabContent>
+        <Button onclick={() => openModal = true}>Tambah Single Master BMHP</Button>
+        <Modal
+          bind:open={openModal}
+          primaryButtonText="Submit"
+          secondaryButtonText="Cancel"
+          on:click:button--primary={submitBMHP}
+          on:click:button--secondary={() => openModal = false}
+        >
+          <TextInput bind:value={inputBMHP} labelText="Nama BMHP" placeholder="Masukkan nama BMHP" />
+          <TextInput bind:value={inputBMHPTags} labelText="Tags" placeholder="pisahkan dengan koma ," />
+        </Modal>
+      </TabContent>
+      <TabContent>
+        <!-- <TabelMasterBMHP /> -->
+      </TabContent>
+    </svelte:fragment>
+  </Tabs>
 {:else}
-Belum login
+  Belum login
 {/if}
